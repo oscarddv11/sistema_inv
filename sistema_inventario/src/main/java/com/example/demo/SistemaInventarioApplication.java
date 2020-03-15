cde6ea89975ddaef288b30b3d6f9a3cafd4c1425
oscarddv11 <38900995+oscarddv11@users.noreply.github.com>
2020-03-14 19:48:24 -0600

    Add files via upload

---
+++ sistema_inventario/src/main/java/com/example/demo/SistemaInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaInventarioApplication.class, args);
+	}
+
+}
---
+++ sistema_inventario/src/main/java/model/entities/r_inventario.java
@@ -0,0 +1,43 @@
+package model.entities;
+
+public class r_inventario {
+
+	private int id;
+	private int codigo;
+	private String desproducto;
+	private int costo;
+	private int precioventa;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getDesproducto() {
+		return desproducto;
+	}
+	public void setDesproducto(String desproducto) {
+		this.desproducto = desproducto;
+	}
+	public int getCosto() {
+		return costo;
+	}
+	public void setCosto(int costo) {
+		this.costo = costo;
+	}
+	public int getPrecioventa() {
+		return precioventa;
+	}
+	public void setPrecioventa(int precioventa) {
+		this.precioventa = precioventa;
+	}
+	
+	
+	
+}
